44ab6395552fdaf0879ce7fd8b91cd37ca1f45f2
Tiffany <tiffany@spaza.tech>
2019-08-02 08:24:57 +0200

    Title and textbox add todo

--- app/src/main/java/com/example/todoapp/AddTodo.kt
+++ app/src/main/java/com/example/todoapp/AddTodo.kt
@@ -8,6 +8,7 @@ class AddTodo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
+
     }
 
 }
