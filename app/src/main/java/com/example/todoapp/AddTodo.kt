a66c38057a58571fc03b8e4fcbaf64b1836f2e31
Tiffany <tiffany@spaza.tech>
2019-08-02 10:09:19 +0200

    add todo page content

--- app/src/main/java/com/example/todoapp/AddTodo.kt
+++ app/src/main/java/com/example/todoapp/AddTodo.kt
@@ -1,7 +1,12 @@
 package com.example.todoapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_add_todo.*
 
 class AddTodo : AppCompatActivity() {
 
@@ -9,6 +14,24 @@ class AddTodo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
 
+        addTodoButton.setOnClickListener { view ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 
 }
